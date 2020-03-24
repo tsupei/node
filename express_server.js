2707cc841f65638a912ac371537b654ffee95bf9
tsupei <tsupei0527@gmail.com>
2020-03-24 21:18:00 +0800

    🎈 New Feature: 引進express

---
+++ express_server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res){
+	res.send("Hello World!");
+});
+
+app.get('/index.html', function(req, res){
+	res.sendFile(__dirname + "/" + "index.html");
+});
+
+var server = app.listen(8888, function(){
+		var host = server.address().address;
+		var port = server.address().port;
+
+		console.log("Server running at http://%s:%s", host, port);
+})
